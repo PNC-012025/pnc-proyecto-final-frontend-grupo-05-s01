1928102737498145a14fa723f6c2ecdc8a35e60d
alexandrappe <00117222@uca.edu.sv>
2025-06-13 23:13:28 -0600

    Add initial admin and user project settings :sparkles:

---
+++ admin/nextadminjs-demo/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import Footer from "./components/Footer";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Mercaduca",
+  description: "Project for Mercaduca",
+  icons: {
+    icon: "/logo.png", 
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="flex flex-col min-h-screen">
+      <main className="flex-grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
