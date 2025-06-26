9e94586f42038d673e15608f5f14b01c027f3140
alexandrappe <00117222@uca.edu.sv>
2025-06-26 17:45:22 -0600

    Improve format of the code :art:

--- admin/nextadminjs-demo/src/app/entrepreneurs/page.jsx
+++ admin/nextadminjs-demo/src/app/entrepreneurs/page.jsx
@@ -16,7 +16,7 @@ const Page = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 9;
 
-   useEffect(() => {
+  useEffect(() => {
     async function fetchBrands() {
       try {
         const queryParams = new URLSearchParams();
@@ -46,20 +46,20 @@ const Page = () => {
     }
 
     fetchBrands();
-  }, [filter, filter2]); 
+  }, [filter, filter2]);
 
- const filteredData = brands.filter(
-  (item) =>
-    item.businessName &&
-    item.businessName.toLowerCase().includes(searchTerm.toLowerCase())
-);
+  const filteredData = brands.filter(
+    (item) =>
+      item.businessName &&
+      item.businessName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-const totalPages = Math.ceil(filteredData.length / itemsPerPage);
-const startIdx = (currentPage - 1) * itemsPerPage;
-const currentItems = filteredData.slice(startIdx, startIdx + itemsPerPage);
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+  const startIdx = (currentPage - 1) * itemsPerPage;
+  const currentItems = filteredData.slice(startIdx, startIdx + itemsPerPage);
 
-if (loading) return <p>Cargando marcas aprobadas...</p>
-if (error) return <p className="text-red-500">{error}</p>
+  if (loading) return <p>Cargando marcas aprobadas...</p>
+  if (error) return <p className="text-red-500">{error}</p>
 
 
   return (
@@ -68,70 +68,70 @@ if (error) return <p className="text-red-500">{error}</p>
         Revisa el estado de los emprendedores y sus talonarios
       </h1>
 
-  <div className="mb-4 w-full flex flex-col items-center gap-4">
-  {/* Search input */}
-  <div className="relative w-full max-w-md text-foreground">
-    <input
-      type="text"
-      placeholder="Buscar marcas"
-      value={searchTerm}
-      onChange={(e) => {
-        setSearchTerm(e.target.value);
-        setCurrentPage(1);
-      }}
-      className="border rounded-full px-4 py-2 pr-10 w-full shadow-md focus:outline-none focus:ring-0 text-foreground font-info"
-    />
-    <FaSearch className="absolute right-4 top-1/2 transform -translate-y-1/2 text-foreground" />
-  </div>
+      <div className="mb-4 w-full flex flex-col items-center gap-4">
+        {/* Search input */}
+        <div className="relative w-full max-w-md text-foreground">
+          <input
+            type="text"
+            placeholder="Buscar marcas"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+              setCurrentPage(1);
+            }}
+            className="border rounded-full px-4 py-2 pr-10 w-full shadow-md focus:outline-none focus:ring-0 text-foreground font-info"
+          />
+          <FaSearch className="absolute right-4 top-1/2 transform -translate-y-1/2 text-foreground" />
+        </div>
 
-  {/* Filter dropdowns */}
-  <div className="flex flex-col sm:flex-row justify-center items-center gap-4 w-full max-w-md">
-    <div className="relative w-full">
-      <select
-        value={filter}
-        onChange={(e) => {
-          setFilter(e.target.value);
-          setCurrentPage(1);
-        }}
-        className="appearance-none border rounded-full pl-3 pr-8 py-2 w-full shadow-md focus:outline-none focus:ring-0 text-foreground font-info"
-      >
-        <option value="">Todos</option>
-        <option value="true">Con contrato</option>
-        <option value="false">Sin contrato</option>
-      </select>
-      <FaChevronDown className="pointer-events-none absolute right-4 top-1/2 transform -translate-y-1/2" />
-    </div>
+        {/* Filter dropdowns */}
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-4 w-full max-w-md">
+          <div className="relative w-full">
+            <select
+              value={filter}
+              onChange={(e) => {
+                setFilter(e.target.value);
+                setCurrentPage(1);
+              }}
+              className="appearance-none border rounded-full pl-3 pr-8 py-2 w-full shadow-md focus:outline-none focus:ring-0 text-foreground font-info"
+            >
+              <option value="">Todos</option>
+              <option value="true">Con contrato</option>
+              <option value="false">Sin contrato</option>
+            </select>
+            <FaChevronDown className="pointer-events-none absolute right-4 top-1/2 transform -translate-y-1/2" />
+          </div>
 
-    <div className="relative w-full">
-      <select
-        value={filter2}
-        onChange={(e) => {
-          setFilter2(e.target.value);
-          setCurrentPage(1);
-        }}
-        className="appearance-none border rounded-full pl-3 pr-8 py-2 w-full shadow-md focus:outline-none focus:ring-0 text-foreground font-info"
-      >
-        <option value="">Todos</option>
-        <option value="ACTIVO">Activo</option>
-        <option value="INACTIVO">Inactivo</option>
-      </select>
-      <FaChevronDown className="pointer-events-none absolute right-4 top-1/2 transform -translate-y-1/2" />
-    </div>
-  </div>
-</div>
+          <div className="relative w-full">
+            <select
+              value={filter2}
+              onChange={(e) => {
+                setFilter2(e.target.value);
+                setCurrentPage(1);
+              }}
+              className="appearance-none border rounded-full pl-3 pr-8 py-2 w-full shadow-md focus:outline-none focus:ring-0 text-foreground font-info"
+            >
+              <option value="">Todos</option>
+              <option value="ACTIVO">Activo</option>
+              <option value="INACTIVO">Inactivo</option>
+            </select>
+            <FaChevronDown className="pointer-events-none absolute right-4 top-1/2 transform -translate-y-1/2" />
+          </div>
+        </div>
+      </div>
 
 
       {/* Cards founds*/}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8">
         {currentItems.map((brand) => (
-          <Entrepeneurcard 
-          key={brand.id} 
-          id={brand.id} 
-          logo={brand.urlLogo}
-          brandName={brand.businessName}
-          responsible={brand.ownerFullName}
-          carnet={brand.ownerEmail}
-          career={brand.major}
+          <Entrepeneurcard
+            key={brand.id}
+            id={brand.id}
+            logo={brand.urlLogo}
+            brandName={brand.businessName}
+            responsible={brand.ownerFullName}
+            carnet={brand.ownerEmail}
+            career={brand.major}
           />
         ))}
       </div>
